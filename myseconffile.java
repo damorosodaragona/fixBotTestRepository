92be1c98f929169528a6b2667b3f990deba878eb
damorosodaragona <darioamorosodaragona@gmail.com>
2021-07-21 00:47:12 +0200

    Update myseconffile.java

--- myseconffile.java
+++ myseconffile.java
@@ -4,15 +4,21 @@
 
 public class try{
 
-   //to fix waiting for https://github.com/damorosodaragona/mypruvaterepository/issues/1
+   // to fix https://github.com/damorosodaragona/fixBotTestRepository/issues/3
    public String _print(String s){
       return s
    }
  
- //to fix waiting for https://github.com/damorosodaragona/mypruvaterepository/issues/2
- public String _print2(String s){
-   return s
+    //https://github.com/damorosodaragona/fixBotTestRepository/issues/2
+    public String _print2(String s){
+      return s
     }
+   
+   //https://github.com/damorosodaragona/fixBotTestRepository/issues/1
+   public String _print2(String s){
+    return s
+    }
+
 
 }
 
